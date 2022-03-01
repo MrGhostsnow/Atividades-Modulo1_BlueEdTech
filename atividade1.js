9422ad0dbf7f3bf17d923d6782a5f7ab4de1dc00
MrGhostsnow <victorio5054@gmail.com>
2022-03-01 15:33:08 -0300

    O maior número

--- atividade1.js
+++ atividade1.js
@@ -1,7 +1,11 @@
 const  prompt  = require ("prompt-sync") ( ) ;
-const nota1 = parseInt(prompt("Digite a 1º nota: "));
-const nota2 = parseInt(prompt("Digite a 2º nota: "));
-nota1 = nota1 * 4;
-nota2 = nota2 * 6;
-let media = (nota1 + nota2) / 2;
-console.log("A media das duas notas é", media)
+const numero1 = +prompt("Digite o 1º número: ");
+const numero2 = +prompt("Digite o 2º número: ");
+if (numero1 > numero2){
+    console.log(`O 1º número (${numero1}) é maior que o 2º número (${numero2})`);
+}   else if(numero2 > numero1){
+        console.log(`O 2º número (${numero2}) é maior que o 1º número (${numero1})`);
+}   else{
+        console.log(`O 1º número (${numero1}) e o 2º número (${numero2}) são IGUAIS`);
+    }
+    
